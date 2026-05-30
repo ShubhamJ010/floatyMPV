40919fb1330d9775330497ac39e48508165be9a9
Shubham Jha <samxn81@gmail.com>
2026-05-31 00:58:33 +0530

    Initial Commit

---
+++ floatyMPV/floatyMPVApp.swift
@@ -0,0 +1,17 @@
+//
+//  floatyMPVApp.swift
+//  floatyMPV
+//
+//  Created by Shubham Jha on 31/05/26.
+//
+
+import SwiftUI
+
+@main
+struct floatyMPVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
